73380249526a881e654b7cd581667fefcb967c42
João Peixoto <joaovictor260921@gmail.com>
2024-02-20 19:05:36 -0300

    modificações no db, formulario.css e .env

--- server/src/db.js
+++ server/src/db.js
@@ -8,10 +8,10 @@ class db {
     async select(sql) {
         try {
             const conection = await mysql.createConnection({
-                host: 'localhost',
-                user: 'root',
-                database: 'igrejas',
-                password: 'MASTERKEY@23'
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                database: process.env.DB_DATABASER,
+                password: process.env.DB_PASSWORD
             });
             
             const res = await conection.query(sql);
@@ -23,7 +23,54 @@ class db {
     }
 
     async insert(sql){
+        let fields = sql.fields
+            , values = sql.values
+            , table = sql.table
+        ;
         
+        // Caso qualquer parametro obrigatorio esteja faltante, retorna um erro.
+        if(!fields || !values || !table){
+            return {
+                erro: true,
+                mensagem: 'Parametros obrigatorios faltantes!'
+            }
+        }
+
+        // Cria lista de fields textual
+        let fields_textal = ''
+        fields.forEach(field => {
+            fields_textal += "'" + field + "',";
+        });
+
+        fields_textal = fields_textal.slice(0, fields_textal.length-1);
+
+        // cria lista de value textual
+        for (let i = 0; i < array.length; i++) {
+            
+            
+        }
+
+        try {
+            const conection = await mysql.createConnection({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                database: process.env.DB_DATABASER,
+                password: process.env.DB_PASSWORD
+            });
+            
+            const res = await conection.query(`
+                INSERT INTO ${table} (${fields_textal})
+                VALUE
+                ()
+
+            `);
+            return res[0];
+            
+        } catch (error) {
+            return error;
+        }
+
+
     }
 }
 
